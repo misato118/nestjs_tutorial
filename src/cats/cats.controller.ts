dee8ff3d8bbae0a996500c2abb629c22d12859ed
misato100 <misato1@my.yorku.ca>
2024-09-16 15:23:38 -0400

    Added a service and connected it to a controller

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
+
+    constructor(private readonly catsService: CatsService) {}
+
     @Get()
-    getCats(): string {
-        return 'Meow! Here are your cute cats.';
+    getCats(): string[] {
+        return this.catsService.getCats();
     }
 }
